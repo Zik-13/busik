7b90652922bf55e32c34f8ab79bad5450e75816e
Zik-13 <zikcruel@mail.ru>
2023-04-20 16:11:28 +0300

    add link

--- exmp1/matchfun/matchGenereate.php
+++ exmp1/matchfun/matchGenereate.php
@@ -9,6 +9,7 @@
     $result = 001;
 
     // (int) $result =  $numOne[0] .  $matcFun[rand(0,4)] . $numTwo[0] . $matcFun[rand(0,4)] . $numThree[0]; // Сборка примера
+    // https://youtu.be/syqMx8e1g4U?t=145 link with lesson
 
     echo $result;
 ?>
