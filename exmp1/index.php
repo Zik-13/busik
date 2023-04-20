30ae86b2c422d68dfcfcb66cdc718d02017db2b0
Zik-13 <zikcruel@mail.ru>
2023-04-20 16:08:51 +0300

    new page

---
+++ exmp1/index.php
@@ -0,0 +1,9 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+?>
+
+    <?
+        require_once("matchfun/matchGenereate.php");
+    ?>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ exmp1/matchfun/matchGenereate.php
@@ -0,0 +1,14 @@
+<?php
+
+    $numOne = array(rand(0,10000));
+    $numTwo = array(rand(0,10000));
+    $numThree = array(rand(0,10000));
+    $matcFunLvlOne = array("+", "-");
+    $matcFunLvlTwo = array("*", "/");
+    $trueAnswer = 0;
+    $result = 001;
+
+    // (int) $result =  $numOne[0] .  $matcFun[rand(0,4)] . $numTwo[0] . $matcFun[rand(0,4)] . $numThree[0]; // Сборка примера
+
+    echo $result;
+?>
