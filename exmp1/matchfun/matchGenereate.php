6abce9b20df1c531e52178ea1e1a30f923c15aa0
Zik-13 <zikcruel@mail.ru>
2023-04-20 16:24:03 +0300

    начал писать функцию, но не работает

--- exmp1/matchfun/matchGenereate.php
+++ exmp1/matchfun/matchGenereate.php
@@ -6,7 +6,24 @@
     $matcFunLvlOne = array("+", "-");
     $matcFunLvlTwo = array("*", "/");
     $trueAnswer = 0;
-    $result = 001;
+    $result = 0;
+    funchek($numOne[0], $numTwo[0], $numThree[0], $matcFunLvlOne[0], $result);
+    echo $result;
+
+    function funchek($num1, $num2, $num3, $mat, $resl)
+    {
+        switch ($mat){
+            case "+":
+                $resl = $num1 + $num2 + $num3;
+                break;
+            case "-":
+                $num1 - $num2 - $num3;
+                break;
+                return $resl;
+        }
+    }
+
+
 
     // (int) $result =  $numOne[0] .  $matcFun[rand(0,4)] . $numTwo[0] . $matcFun[rand(0,4)] . $numThree[0]; // Сборка примера
     // https://youtu.be/syqMx8e1g4U?t=145 link with lesson
